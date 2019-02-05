730d4af91415d4d05240f16b63537e043feeb525
freddyt3 <fred.tidwell.03@gmail.com>
2019-02-04 21:56:50 -0800

    added login logic

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -25,4 +30,4 @@ app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port: ${port}`));
